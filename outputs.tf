808408537e7be5263bd6b48b9b311fd015664c3f
christian-calabrese <christian.calabrese@outlook.com>
2023-12-11 14:55:51 +0100

    feat: variable_ids output added

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,10 @@ output "id" {
   description = "The ID of the variable set"
   value       = tfe_variable_set.this.id
 }
+
+output "variable_ids" {
+  description = "The ID of the variables contained in the variable set"
+  value = {
+    for key, variable in tfe_variable.this : key => variable.id
+  }
+}
